bb773275a0c5d5ea3a121080d4785afca8bf239a
Canzi <99710802+CanziWan@users.noreply.github.com>
2022-04-09 18:31:05 +0800

    Update #1 两数之和.c

--- algorithm/LeetCode/Java/#1 两数之和.c
+++ algorithm/LeetCode/Java/#1 两数之和.c
@@ -6,22 +6,24 @@
 
 ## 代码
 
-Note: The returned array must be malloced, assume caller calls free().
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-	for(int i = 0; i < numsSize; i++){
-   	 int x = nums[i];
- 	   for(int j = i + 1; j < numsSize; j++){
-     	   if(nums[j] == target - x){
-           	 int* ret = (int*)malloc(sizeof(int)*2);
-         	   ret[0] = i;
-         	   ret[1] = j;
-            	*returnSize = 2;
-          	  return ret;
-       	 }
-  	  }
-	}
-	*returnSize = 0;
-	return NULL;
+    for(int i = 0; i < numsSize; i++){
+        int x = nums[i];
+        for(int j = i + 1; j < numsSize; j++){
+            if(nums[j] == target - x){
+                int* ret = (int*)malloc(sizeof(int)*2);
+                ret[0] = i;
+                ret[1] = j;
+                *returnSize = 2;
+                return ret;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
 }
+
 
